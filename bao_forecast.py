91a03e622a1095b199301df54f3c3aff89b111d3
Juan Mena Fernandez <juanmena95@hotmail.es>
2026-05-28 03:46:59 -0700

    Improved description of the code

--- bao_forecast.py
+++ bao_forecast.py
@@ -41,8 +41,6 @@ if pk_implementation == "old":
 
     sigma0 = 12.4 * cosmo.sigma8_m / 0.9 * 0.758 # the 12.4 is in Mpc/h. The 0.9 is sigma_8. See page 4 of https://arxiv.org/pdf/astro-ph/0701079
 
-    amp_BAO = 0.5 # approximate, see page 10 of https://arxiv.org/pdf/astro-ph/0701079 for details
-
 elif pk_implementation == "new":
 
     pkz = cosmo.get_fourier(engine="class").pk_interpolator()
@@ -72,7 +70,7 @@ elif pk_implementation == "new":
     print(f"Value used by Seo & Eisenstein (2007): {(12.4 * cosmo.sigma8_m / 0.9 * 0.758):.2f} Mpc/h")
     print()
     
-    amp_BAO = 0.4529 # approximate, see page 10 of https://arxiv.org/pdf/astro-ph/0701079 for details
+amp_BAO = 0.5 # approximate, see page 10 of https://arxiv.org/pdf/astro-ph/0701079 for details
 
 mu_vector = np.linspace(0.0, 1.0, 10**4)
 
@@ -94,7 +92,7 @@ def IndividualBAOError(z_min, z_max, n_gal, sigma_z, bias, area):
     volume = (4.0 * np.pi / 3.0 * fsky * (chi_max**3 - chi_min**3)) # in (Mpc/h)^3
 
     # -----------------------------------------------------
-    # Growth + RSD
+    # Shotnoise parameter
     # -----------------------------------------------------
 
     D = cosmo.growth_factor(z)
@@ -107,20 +105,14 @@ def IndividualBAOError(z_min, z_max, n_gal, sigma_z, bias, area):
     nP = nbar * power # dimensionless
 
     # -----------------------------------------------------
-    # BAO damping from non-linear evolution
+    # Non-linear BAO damping
     # -----------------------------------------------------
 
     sigma_par = sigma0 * D * (1.0 + f)
     sigma_per = sigma0 * D
-    sigma_z_dist = 2997.92458 / cosmo.efunc(z) * sigma_z # in Mpc/h
+    sigma_z_dist = 299792.458 / cosmo.hubble_function(z) * sigma_z * cosmo.h # in Mpc/h
     sigma_z_bao = sigma_z_dist / cosmo.comoving_radial_distance(z) * cosmo.rs_drag # in Mpc/h
     sigma2_tot = (sigma_par**2 * mu_vector**2 + sigma_per**2 * (1.0 - mu_vector**2) + 0.5 * sigma_z_bao**2) # in (Mpc/h)^2
-
-    # -----------------------------------------------------
-    # BAO damping from photo-z smearing
-    # -----------------------------------------------------
-
-    # sigzdampl = BAOdampsigz(z, sigma_z, nthbin=len(kh_vector)) # This was in Ashley's original code, but it was never used
 
     # -----------------------------------------------------
     # Silk damping
@@ -129,10 +121,21 @@ def IndividualBAOError(z_min, z_max, n_gal, sigma_z, bias, area):
     silk = np.exp(-2.0 * (kh_vector * silk_BAO)**1.4)
     
     # -----------------------------------------------------
-    # Double integral (with BAO component split)
+    # RSD term with photo-z smearing
     # -----------------------------------------------------
     
     R = (1.0 + beta * mu_vector**2)**2 * np.exp(-kh_vector[:, None]**2 * mu_vector**2 * sigma_z_dist**2) # eq. 27 from https://arxiv.org/pdf/astro-ph/0701079. Dimensionless
+
+    # -----------------------------------------------------
+    # Extra BAO damping from photo-z smearing
+    # -----------------------------------------------------
+
+    # sigzdampl = BAOdampsigz(z, sigma_z, nthbin=len(kh_vector)) # This was in Ashley's original code, but it was never used
+    
+    # -----------------------------------------------------
+    # All combined
+    # -----------------------------------------------------
+    
     denom = pk_BAO[:, None] + 1.0 / (nP * R) # dimensionless
     
     # base = kh_vector[:, None]**2 * silk[:, None] * np.exp(-kh_vector[:, None]**2 * sigma2_tot) * sigzdampl[:, None]**2 / denom**2 # in (h/Mpc)^2
@@ -150,7 +153,7 @@ def IndividualBAOError(z_min, z_max, n_gal, sigma_z, bias, area):
     Fhh = np.trapz(np.trapz(Fhh_integrand, mu_vector), kh_vector) # in (h/Mpc)^3
     Fdh = np.trapz(np.trapz(Fdh_integrand, mu_vector), kh_vector) # in (h/Mpc)^3
     
-    prefactor = (amp_BAO**2 * volume / 2.0) # in (Mpc/h)^3. The factor 1/2 is not in eq. 26 of https://arxiv.org/pdf/astro-ph/0701079, but it can be found in eq. 25 (I think they forgot it in 26)
+    prefactor = (amp_BAO**2 * volume / 2.0) # in (Mpc/h)^3. The factor 1/2 is not in eq. 26 of https://arxiv.org/pdf/astro-ph/0701079, but it is in eq. 25 (I think they forgot it in 26)
     
     Fdd *= prefactor # dimensionless
     Fhh *= prefactor # dimensionless
